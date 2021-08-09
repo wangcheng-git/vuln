f6cf8ec12a633271f50a68f35ee60b6f69cdae63
wdan <wdan@git.com>
2021-08-10 01:06:25 +0800

    update index.php

--- html/index.php
+++ html/index.php
@@ -1,9 +1,5 @@
 <?php
-header('Location: public');
-//禁用错误报告，也就是不显示错误
 error_reporting(0);
-//报告运行时错误
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-//报告所有错误
-error_reporting(E_ALL);
-?>
+header('Location: public');
+error_reporting(0);
+
